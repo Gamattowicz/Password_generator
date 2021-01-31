1d7652dd542abc097ce0132a54000f2584bc00aa
Gamattowicz <p.romanczuk31@gmail.com>
2021-01-31 16:46:20 +0100

    add range of character to generate

---
+++ main.py
@@ -0,0 +1,30 @@
+# import random
+
+#Special characters 
+print('SPECIAL CHARACTERS')   
+for i in range(33, 48):
+    print(i, chr(i))
+    
+for i in range(58, 65):
+    print(i, chr(i))
+    
+for i in range(91, 97):
+    print(i, chr(i))
+    
+for i in range(123, 127):
+    print(i, chr(i))    
+    
+#Uppercase characters
+print('UPPERCASE CHARACTERS')
+for i in range(65, 91):
+    print(i, chr(i))
+    
+#Lowercase characters
+print('LOWERCASE CHARACTERS')
+for i in range(97, 123):
+    print(i, chr(i))
+    
+#Numbers
+print('NUMBERS')
+for i in range(48, 58):
+    print(i, chr(i))  
